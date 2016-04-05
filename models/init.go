a37db4be609f6bf467f74fd0c7e4e33ca6870177
Cyrus Forbes <forbescyrus@gmail.com>
2016-04-05 14:07:51 -0500

    Added routes scaffolding

--- models/init.go
+++ models/init.go
@@ -5,10 +5,20 @@ import (
   _ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
+var DB *gorm.DB
+
 func GetDB() *gorm.DB {
   db, err := gorm.Open("postgres", "user=postgres dbname=jukebox password=bugzzues sslmode=disable")
   if err != nil {
     panic(err)
   }
   return db
+}
+
+func init() {
+  var err interface{}
+  DB, err = gorm.Open("postgres", "user=postgres dbname=jukebox password=bugzzues sslmode=disable")
+  if err != nil {
+    panic(err)
+  }
 }
---
+++ routes/song.go
@@ -0,0 +1,50 @@
+package routes
+
+import (
+  "encoding/json"
+  "net/http"
+
+  "github.com/gorilla/mux"
+
+  "github.com/eecs394-s16/brown-api-golang/models"
+
+)
+
+func addSongRoutes(r *mux.Router) {
+  r.HandleFunc("/songs", getSongsHandler).Methods("GET")
+  r.HandleFunc("/songs", createSongHandler).Methods("POST")
+}
+
+func getSongsHandler(w http.ResponseWriter, req *http.Request) {
+  // Get songs
+  var songs []models.Song
+  models.DB.Find(&songs)
+
+  // Create response
+  w.Header().Set("Content-Type", "application/json")
+
+  data := make(map[string]interface{})
+  data["songs"] = songs
+  json.NewEncoder(w).Encode(&data)
+}
+
+func createSongHandler(w http.ResponseWriter, req *http.Request) {
+
+  // Get JSON request data
+  decoder := json.NewDecoder(req.Body)
+  var song models.Song
+  err := decoder.Decode(&song)
+  if err != nil {
+    panic(err)
+    return
+  }
+
+  // Save song
+  if err := models.DB.Create(&song); err != nil {
+    panic(err)
+    return
+  }
+
+  // Create response
+  json.NewEncoder(w).Encode(&song)
+}
