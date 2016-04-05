dfa22437e0437242fe5824bd7a48bfce72bf0fef
Cyrus Forbes <cyrusaf@cyrus-laptop-15>
2016-04-05 14:19:20 -0500

    Fixed create song route

--- models/init.go
+++ models/init.go
@@ -1,23 +1,17 @@
 package models
 
 import (
+  "os"
+
   "github.com/jinzhu/gorm"
   _ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
 var DB *gorm.DB
 
-func GetDB() *gorm.DB {
-  db, err := gorm.Open("postgres", "user=postgres dbname=jukebox password=bugzzues sslmode=disable")
-  if err != nil {
-    panic(err)
-  }
-  return db
-}
-
 func init() {
   var err interface{}
-  DB, err = gorm.Open("postgres", "user=postgres dbname=jukebox password=bugzzues sslmode=disable")
+  DB, err = gorm.Open("postgres", "user=" + os.Getenv("PG_USER") + " dbname=jukebox password=" + os.Getenv("PG_PASSWORD") + " sslmode=disable")
   if err != nil {
     panic(err)
   }
--- scripts/reset_db.go
+++ scripts/reset_db.go
@@ -7,27 +7,25 @@ import (
 )
 
 func main() {
-  db := models.GetDB()
-
   // Add models here...
   _models := []interface{} {
     &models.Song{}}
 
   fmt.Println("\nDropping tables...")
   for _, model := range _models {
-    if !db.HasTable(model) {
+    if !models.DB.HasTable(model) {
         fmt.Printf("* Skipping %T because it does not exist\n", model)
         continue
     }
     fmt.Printf("* Dropping %T...", model)
-    db.DropTable(model)
+    models.DB.DropTable(model)
     fmt.Printf(" Dropped!\n")
   }
 
   fmt.Println("\nCreating tables...")
   for _, model := range _models {
     fmt.Printf("* Creating %T...", model)
-    db.CreateTable(model)
+    models.DB.CreateTable(model)
     fmt.Printf(" Created!\n")
   }
 
