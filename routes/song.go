7b729efe9e8cecefd5cda9ca839986282f6369bd
Cyrus Forbes <forbescyrus@gmail.com>
2016-04-12 13:37:21 -0500

    upvote and delete songs

--- routes/song.go
+++ routes/song.go
@@ -15,21 +15,8 @@ func addSongRoutes(r *mux.Router) {
   r.HandleFunc("/songs", getSongsHandler).Methods("GET")
   r.HandleFunc("/songs", createSongHandler).Methods("POST")
 
-  r.HandleFunc("/songs/{song_id}/upvote", upvoteSongHandler).Methods("POST") // TODO
-  r.HandleFunc("/songs/{song_id}", deleteSongHandler).Methods("DELETE") // TODO
-}
-
-// TODO
-func deleteSongHandler(w http.ResponseWriter, req *http.Request) {
-  song_id := mux.Vars(req)["song_id"]
-  fmt.Println(song_id)
-
-  // Get song by id
-
-  // Delete song
-
-  // Return response
-   w.Write([]byte("delete songs!\n"))
+  r.HandleFunc("/songs/{song_id}/upvote", upvoteSongHandler).Methods("PUT")
+  r.HandleFunc("/songs/{song_id}", deleteSongHandler).Methods("DELETE")
 }
 
 func getSongsHandler(w http.ResponseWriter, req *http.Request) {
@@ -66,20 +53,47 @@ func createSongHandler(w http.ResponseWriter, req *http.Request) {
 
 func upvoteSongHandler(w http.ResponseWriter, req *http.Request) {
   song_id := mux.Vars(req)["song_id"]
-  fmt.Println(song_id)
 
   var song models.Song
-  models.DB.Where("ID = ?", song_id)
+  models.DB.First(&song, song_id)
 
   // Check that song exists
   if models.DB.NewRecord(song) {
     fmt.Println("cannot find song")
+    return
   }
   fmt.Println("found song")
 
   // +1 to score
+  song.Votes++
 
   // Save song
+  models.DB.Save(&song)
 
   // Return song in response
+  json.NewEncoder(w).Encode(&song)
+}
+
+func deleteSongHandler(w http.ResponseWriter, req *http.Request) {
+  song_id := mux.Vars(req)["song_id"]
+
+  // Get song by id
+  var song models.Song
+  models.DB.First(&song, song_id)
+
+  // Check that song exists
+  if models.DB.NewRecord(song) {
+    fmt.Println("cannot find song")
+    return
+  }
+  fmt.Println("found song")
+
+  // Delete song
+  models.DB.Delete(&song)
+
+  data := make(map[string]interface{})
+  data["deleted"] = song_id
+
+  // Return response
+  json.NewEncoder(w).Encode(&data)
 }
