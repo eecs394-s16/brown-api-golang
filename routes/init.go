492ca5654680f250310ad5d27b84c05fbe073cdb
Cyrus Forbes <forbescyrus@gmail.com>
2016-04-12 21:51:31 -0500

    added allow headers

--- routes/init.go
+++ routes/init.go
@@ -23,5 +23,7 @@ func configureResponseMiddleware(w http.ResponseWriter, req *http.Request, next 
   w.Header().Set("Content-Type", "application/json")
   w.Header().Set("Access-Control-Allow-Origin", "*")
   w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+  w.Header().Set("Access-Control-Allow-Headers",
+    "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
   next(w, req)
 }
