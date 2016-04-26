42e7fd289f0cfd7eb9ee2ea0fc830822df01ea34
Cyrus Forbes <forbescyrus@gmail.com>
2016-04-26 12:12:19 -0500

    added profiling

--- main.go
+++ main.go
@@ -2,9 +2,15 @@ package main
 
 import (
   "github.com/eecs394-s16/brown-api-golang/routes"
+  _ "net/http/pprof"
+  "net/http"
+  "log"
 )
 
 func main() {
+  go func() {
+    log.Println(http.ListenAndServe("localhost:6060", nil))
+  }()
   n  := routes.GetRouter()
   n.Run(":3000")
 }
