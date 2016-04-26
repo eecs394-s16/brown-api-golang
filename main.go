596869abe274e6a4f2cb9cf314efe4b81f54b9b1
Cyrus Forbes <forbescyrus@gmail.com>
2016-04-26 12:13:59 -0500

    added profiling

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
   go func() {
-    log.Println(http.ListenAndServe("localhost:6060", nil))
+    log.Println(http.ListenAndServe("0.0.0.0:6060", nil))
   }()
   n  := routes.GetRouter()
   n.Run(":3000")
